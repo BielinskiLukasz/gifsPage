86df1b34a7e7a1dd9aeae2fb58617c1f980b3009
Lukas <test@local.com>
2018-02-03 12:28:07 +0100

    added static and templates

--- src/main/java/com/bielinski/kwejk/contoller/HomeController.java
+++ src/main/java/com/bielinski/kwejk/contoller/HomeController.java
@@ -1,13 +1,13 @@
 package com.bielinski.kwejk.contoller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello all";
+        return "home";
     }
 }
