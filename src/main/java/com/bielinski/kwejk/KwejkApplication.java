f36207c9d630287e95e8ca5eb60d3ceed5bca10c
Lukas <test@local.com>
2018-02-03 10:13:02 +0100

    Initial commit

---
+++ src/main/java/com/bielinski/kwejk/KwejkApplication.java
@@ -0,0 +1,12 @@
+package com.bielinski.kwejk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwejkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwejkApplication.class, args);
+	}
+}
