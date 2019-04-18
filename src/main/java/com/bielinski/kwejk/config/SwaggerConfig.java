85868e5b6ee9f3d66b8707042d849516beb9b40c
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-04-18 22:23:50 +0200

    Customize swagger

--- src/main/java/com/bielinski/kwejk/config/SwaggerConfig.java
+++ src/main/java/com/bielinski/kwejk/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.bielinski.kwejk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +20,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("gif-bieluk")
+                .version("1.00")
+                .description("Web app to display gifs from internal image library. Options to search gifs by name or " +
+                        "category, select and show favorites, copy url form selected gif.")
+                .contact(new Contact("Łukasz Bieliński",
+                        "https://www.linkedin.com/in/lukasz-bielinski/",
+                        "l.p.bielinski@gmail.com"))
                 .build();
     }
 }
